370a3f4133a1857bb24f8a19c802332c2141ffb8
mkang987 <mkang987@gmail.com>
2022-02-15 19:30:21 -0500

    edited controller index, renamed view/layout to layoutS

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,9 +3,7 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
-  // console.log(req.session);
-  // res.sendFile(path.join(__dirname, './views/layout/main'))
-  res.render("login");
+  res.render("homepage");
 });
 
 module.exports = router;
