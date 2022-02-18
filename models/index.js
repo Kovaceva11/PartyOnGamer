9c9ad476a50587729bdf60287c0b6157560bc294
The Kovac <88350033+Kovaceva11@users.noreply.github.com>
2022-02-17 20:54:10 -0500

    commenting out index.js for heroku testing

--- models/index.js
+++ models/index.js
@@ -1,19 +1,19 @@
 const User = require("./User");
-const Profile = require("./Profile");
-// const Merch = require("./Merch");
-const Post = require("./Post");
-const Comments = require("./Comments");
+// const Profile = require("./Profile");
+// // const Merch = require("./Merch");
+// const Post = require("./Post");
+// const Comments = require("./Comments");
 
-Profile.hasMany(Post, {
-    foreignKey: 'profile_id',
-});
+// Profile.hasMany(Post, {
+//     foreignKey: 'profile_id',
+// });
 
-Profile.hasMany(Comments, {
-    foreignKey: 'comments_id',
-});
+// Profile.hasMany(Comments, {
+//     foreignKey: 'comments_id',
+// });
 
-Profile.belongsTo(User, {
-    foreignKey: 'user_id',
-})
+// Profile.belongsTo(User, {
+//     foreignKey: 'user_id',
+// })
 
-module.exports = { User, Post, Comments, Profile };
+module.exports = { User };
